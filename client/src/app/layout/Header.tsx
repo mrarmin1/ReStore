df780627b03323b4c1217d9d588a05920a8969c8
Armin <armin.pezo@gmail.com>
2021-12-06 15:02:00 +0100

    Creating a component with product details

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,12 +1,76 @@
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, Badge, Box, IconButton, List, ListItem, Toolbar, Typography } from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
+import { NavLink } from "react-router-dom";
+
+const midLinks = [
+    {title: 'catalog', path: '/catalog'},
+    {title: 'contact', path: '/contact'},
+    {title: 'home', path: '/'},
+]
+const rightLinks = [
+    {title: 'login', path: '/login'},
+    {title: 'register', path: '/register'},
+]
 
 export default function Header() {
     return (
         <AppBar position='static' sx={{mb: 4}}>
-            <Toolbar>
+            <Toolbar sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+                <Box display='flex' alignItems='center'>
                 <Typography variant='h6'>
                     React-STORE
                 </Typography>
+                </Box>
+                <List sx={{display: 'flex'}}>
+                    {midLinks.map(({title, path}) => (
+                        <ListItem
+                            component={NavLink}
+                            to={path}
+                            key={path}
+                            sx={{
+                                color: 'inherit', 
+                                typography: 'h6',
+                                '&:hover': {
+                                    color: 'grey.500'
+                                },
+                                '&.active': {
+                                    color: 'text.secondary'
+                                }
+                            }}
+                        >
+                            {title.toUpperCase()}
+                        </ListItem>
+                    ))}
+                </List>
+
+                <Box display='flex' alignItems='center'>
+                <IconButton size='large' sx={{color: 'inherit'}}>
+                    <Badge badgeContent={4} color='secondary'>
+                            <ShoppingCart />
+                    </Badge>
+                </IconButton>
+                <List sx={{display: 'flex'}}>
+                    {rightLinks.map(({title, path}) => (
+                        <ListItem
+                            component={NavLink}
+                            to={path}
+                            key={path}
+                            sx={{
+                                color: 'inherit', 
+                                typography: 'h6',
+                                '&:hover': {
+                                    color: 'grey.500'
+                                },
+                                '&.active': {
+                                    color: 'text.secondary'
+                                }
+                            }}
+                        >
+                            {title.toUpperCase()}
+                        </ListItem>
+                    ))}
+                  </List>
+               </Box>
             </Toolbar>
         </AppBar>
     )
--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,5 +1,69 @@
+import { Divider, Grid, TableBody, Table, TableContainer, TableRow, Typography, TableCell } from "@material-ui/core";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+
+import { Product } from "../../app/models/product";
+
 export default function ProductDetails() {
+    
+const {id} = useParams<{id: string}>();
+const [product, setProduct] = useState<Product | null>(null);
+const [loading, setLoading] = useState(true);
+
+useEffect(() => {
+    axios.get(`http://localhost:5000/api/products/${id}`)
+         .then(response => setProduct(response.data))
+         .catch(error => console.log(error))
+         .finally(() => setLoading(false));
+   }, [id])
+
+   if (loading) return <h3>Loading</h3>
+   if(!product) return <h3>Product not found</h3>
+    
     return (
-        <h1>Product Details</h1>
+       <Grid container spacing={6}>
+            <Grid item xs={6}>
+                 <img src={product.pictureUrl} alt={product.name} style={{width: '100%'}}/>
+            </Grid>
+            <Grid item xs={6}>
+                 <Typography variant='h3'>{product.name}</Typography>
+                 <Divider sx={{mb: 2}} />
+                 <Typography variant='h5' color='secondary'>{product.price.toFixed(2)} BAM</Typography>
+                 <TableContainer>
+                      <Table>
+                           <TableBody>
+                                <TableRow>
+                                       <TableCell>Name</TableCell>
+                                       <TableCell>{product.name}</TableCell>
+                                       
+                                </TableRow>
+                                <TableRow>
+                                       <TableCell>Descriprion</TableCell>
+                                       <TableCell>{product.description}</TableCell>
+                                       
+                                </TableRow>
+                                <TableRow>
+                                       <TableCell>Type</TableCell>
+                                       <TableCell>{product.type}</TableCell>
+                                       
+                                </TableRow>
+                                <TableRow>
+                                       <TableCell>Brand</TableCell>
+                                       <TableCell>{product.brand}</TableCell>
+                                       
+                                </TableRow>
+                                <TableRow>
+                                       <TableCell>Quantity in stock</TableCell>
+                                       <TableCell>{product.quantityInStock}</TableCell>
+                                       
+                                </TableRow>
+                           </TableBody>
+                      </Table>
+                 </TableContainer>
+            </Grid>
+            
+       </Grid> 
     )
 }
