4f042a5c03ec3c6c00b5a7073b7e616cb6c1302c
Armin <armin.pezo@gmail.com>
2021-12-09 22:46:28 +0100

    search and sort functionality on client page

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,23 +1,82 @@
+import { Box, Checkbox, FormControl, FormControlLabel, FormGroup, Grid, Pagination, Paper, Radio, RadioGroup, Typography } from "@material-ui/core";
 import { useEffect } from "react";
+import RadioButtonGroup from "../../app/components/RadioButtonGroup";
 import LoadingComponent from "../../app/layout/LoadingComponent";
 import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
-import { fetchProductsAsync, productSelectors } from "./catalogSlice";
+import { fetchFilters, fetchProductsAsync, productSelectors, setProductParams } from "./catalogSlice";
 import ProductList from "./ProductList";
+import ProductSearch from "./ProductSearch";
 
+const sortOptions = [
+  {value: 'name', label: 'Alphabetical'},
+  {value: 'priceDesc', label: 'Price - High to low'},
+  {value: 'price', label: 'Price - Low to high'},
+]
 
 export default function Catalog() {  
     const products = useAppSelector(productSelectors.selectAll);
-    const {productsLoaded, status} = useAppSelector(state => state.catalog);
+    const {productsLoaded, status, filtersLoaded, brands, types, productParams} = useAppSelector(state => state.catalog);
     const dispatch = useAppDispatch();
  
 
     useEffect(() => {
       if(!productsLoaded) dispatch(fetchProductsAsync());
     }, [productsLoaded, dispatch]) 
+
+    useEffect(() => {
+      if(!filtersLoaded) dispatch(fetchFilters());
+    }, [dispatch, filtersLoaded])
+
     if(status.includes('pending')) return <LoadingComponent message='Loading products ...'/>
     return(
-    <>      
-        <ProductList products={products}/>
-    </>
+    <Grid container spacing={4}> 
+        <Grid item xs={3}>
+            <Paper sx={{mb: 2}}>
+               <ProductSearch />
+            </Paper>  
+            <Paper sx={{mb: 2, p: 2}}>
+                 <RadioButtonGroup 
+                  selectedValue={productParams.orderBy}
+                  options={sortOptions}
+                  onChange={(e) => dispatch(setProductParams({orderBy: e.target.value}))}
+                 
+                 />
+            </Paper>
+            <Paper sx={{mb: 2, p: 2}}>
+               <FormGroup>
+                 {brands.map(brand => (
+                       <FormControlLabel control={<Checkbox />} label={brand} key={brand} />
+                 ))}
+ 
+               </FormGroup>  
+            </Paper>  
+            <Paper sx={{mb: 2, p: 2}}>
+               <FormGroup>
+                 {types.map(type => (
+                       <FormControlLabel control={<Checkbox />} label={type} key={type} />
+                 ))}
+ 
+               </FormGroup>  
+            </Paper> 
+        </Grid>
+        <Grid item xs={9}>
+            <ProductList products={products}/>
+        </Grid>
+        <Grid item xs={3} />
+        <Grid item xs={9}>
+            <Box display='flex' justifyContent='space-between' alignItems='center'>
+                <Typography>
+                     Displaying 1-6 of 20 items  
+                </Typography> 
+                <Pagination 
+                   color='secondary'
+                   size='large'
+                   count={10}
+                   page={2}
+                   /> 
+            </Box>  
+        </Grid>     
+        
+    </Grid>
     )
 }
